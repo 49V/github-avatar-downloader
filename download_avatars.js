a9b52f564738458327a24ec4632c4fb1c48c0af8
49V <moshelawlor@gmail.com>
2019-01-14 17:55:59 -0800

    Implement basic request with Git API Token in getRepoContributors()

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,22 @@
 var request = require('request');
+var secrets = require('./secrets');
 
 function getRepoContributors (repoOwner, repoName, callback) {
-  
+  var options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `token ${secrets.GITHUB_TOKEN}`
+    }
+  }
+  request (options, function (error, response, body) {
+    callback(error, body);
+  });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result){ 
-  console.log(`Errors: ${err}`);
+getRepoContributors("jquery", "jquery", function(error, result){ 
+  console.log(`Errors: ${error}`);
   console.log(`Result: ${result}`);
 });
 
